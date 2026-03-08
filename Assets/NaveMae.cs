4fd316f61d6a123357c267aed44e03a832c8e63d
nuno <nunomargsilva@gmail.com>
2026-03-08 18:20:03 -0300

    Nava mãe em processo

--- Assets/NaveMae.cs
+++ Assets/NaveMae.cs
@@ -5,18 +5,20 @@ public class NaveMae : MonoBehaviour
     public float velocidade = 5f;
     public int pontosBonus = 10;
 
-    private Rigidbody2D rb;
-
-    void Start()
+    void Update()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.linearVelocity = Vector2.right * velocidade;
+        transform.Translate(Vector2.right * velocidade * Time.deltaTime);
     }
 
     public void AcertouNaveMae()
     {
         gameManager.instance.pontos += pontosBonus;
         gameManager.instance.AtualizarUI();
+        Destroy(gameObject);
+    }
+
+    void OnBecameInvisible()
+    {
         Destroy(gameObject);
     }
 }
